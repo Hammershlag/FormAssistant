bb3e21d51247dc97658f3e9b201bc551233a4766
tomek <tomekzbroszczyk@gmail.com>
2025-05-05 19:08:36 +0200

    Initialize project using Spring Boot for backend and React for frontend

---
+++ form-assistant-backend/src/main/java/com/hammershlag/formassistantbackend/FormAssistantBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hammershlag.formassistantbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormAssistantBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormAssistantBackendApplication.class, args);
+    }
+
+}
---
+++ form-assistant-backend/src/test/java/com/hammershlag/formassistantbackend/FormAssistantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hammershlag.formassistantbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormAssistantBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
